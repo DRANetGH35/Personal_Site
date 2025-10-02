3752cf95e18683b7bb61b78f965ef2bf0f4a5de4
DRANetGH35 <144178956+DRANetGH35@users.noreply.github.com>
2025-10-02 10:55:50 -0700

    add: register functional

--- routes.py
+++ routes.py
@@ -3,9 +3,16 @@ from flask_login import current_user
 from extensions import db
 from models import User
 from app import create_app
+from werkzeug.security import check_password_hash, generate_password_hash
+
+def user_exists(username):
+    try:
+        if db.session.execute(db.select(User).where(User.name == username)).scalar():
+            return True
+    except AttributeError:
+        return False
 
 app = create_app()
-
 @app.route('/')
 def index():
     logged_in = False
@@ -19,14 +26,19 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    error=''
     if request.method == "POST":
+        username = request.form.get('username')
         entered_password = request.form.get('password')
         password_confirmation = request.form.get('confirm_password')
-        if entered_password == password_confirmation:
-            #enter user info into database
-            pass
+        print(username, entered_password, password_confirmation)
+        if entered_password != password_confirmation:
+            error = 'Passwords do not match'
+        elif user_exists(username):
+            error = 'account with this username already exists'
         else:
-            print('passwords dont match')
-        return render_template('register.html')
-    else:
+            db.session.add(User(name=username, password=generate_password_hash(entered_password, method="pbkdf2:sha256", salt_length=8), is_admin=False))
+            db.session.commit()
+        return render_template('register.html', error=error)
+    elif request.method == 'GET':
         return render_template('register.html')
